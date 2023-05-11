161dc9b534ed91c2b16ff6dc811ea93470d8e375
ShaikAdhil <adhil.rehan@gmail.com>
2023-05-11 13:53:05 +0000

    Styled Components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Heading, Button} from './styledComponents'
+
+// import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello Adhil</Heading>
+    <Button type="button">Click</Button>
+    <Button type="button" outline>
+      Click
+    </Button>
+  </>
+)
+
+export default App
